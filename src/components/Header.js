706c7145b96108acb7dd9dfe35eddd4dc666f182
koushith <koushith97@gmail.com>
2021-04-21 11:32:11 +0530

    updates broken link

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ function Header() {
         <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center'></nav>
         <a
           className='inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0'
-          href='https://github.com/Koushith/BTC-Price-Tracker'
+          href='https://github.com/Koushith/BTC-Tracker'
           target='_next'
         >
           View Code
